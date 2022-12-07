4e9ebb97b4506f310ba61e4442954441aba0a40a
Giovana-Manuquian <103048833+Giovana-Manuquian@users.noreply.github.com>
2022-12-07 12:05:16 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
 
 using Backend_SENAI.classes;
@@ -7,14 +7,27 @@ PessoaFisica novaPf = new PessoaFisica();
 
 novaPf.nome = "Giovana";
 novaPf.cpf = "12345678975";
+novaPf.rendimento = 7000.5f;
+Console.WriteLine($"Pessoa Fisica____________________________");
+float resultado = novaPf.CalcularImposto(novaPf.rendimento);
+
+Console.WriteLine(resultado);
 
 // Console.WriteLine(novaPf.nome);
 // Console.WriteLine(novaPf.cpf);
-Console.WriteLine($"------------------------");
+// Console.WriteLine($"------------------------");
 
 
-Console.WriteLine($"Nome {novaPf.nome}, CPF: {novaPf.cpf}");
+// Console.WriteLine($"Nome {novaPf.nome}, CPF: {novaPf.cpf}");
 // interpolação
 
-Console.WriteLine("Bem Vindo" + novaPf.nome + ", CPF" + novaPf.cpf + "");
+// Console.WriteLine("Bem Vindo" + novaPf.nome + ", CPF" + novaPf.cpf + "");
 // concatenação
+
+Console.WriteLine($"Pessoa Juridica____________________________");
+PessoaJuridica novaPj = new PessoaJuridica();
+
+float impostoPagar = novaPj.CalcularImposto(10000.5f);
+Console.WriteLine($"{impostoPagar:0.0}");
+Console.WriteLine($"{impostoPagar.ToString("C")}");
+
