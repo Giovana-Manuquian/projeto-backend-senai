1d784b871062f44fbd1bd91041d3289a9d440e54
Giovana-Manuquian <103048833+Giovana-Manuquian@users.noreply.github.com>
2022-12-07 12:07:32 -0300

    Update PessoaFisica.cs

--- classes/PessoaFisica.cs
+++ classes/PessoaFisica.cs
@@ -14,7 +14,26 @@ namespace Backend_SENAI.classes
 
         public override float CalcularImposto(float rendimento)
         {
-            throw new NotImplementedException();
+            if (rendimento <= 1500)
+            {
+                return 0;
+            }
+            else if (rendimento > 1500 && rendimento <= 3500)
+            {
+                float resultado = (resultado = rendimento / 100) * 2;
+                return resultado;
+            }
+            else if (rendimento > 3500 && rendimento <= 6000 )
+            {
+               float resultado = (resultado = rendimento / 100) * 3.5f;
+               return resultado;
+            }
+            else
+            {
+                float resultado = (resultado = rendimento / 100) * 5;
+                return resultado;
+            }
+            // throw new NotImplementedException();
         }
 
         public bool ValidarDataNasc(DateTime dataNasc)
