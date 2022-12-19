087eb2e1e1740194df388bf370e8fca5c9d7f8ad
Giovana-Manuquian <103048833+Giovana-Manuquian@users.noreply.github.com>
2022-12-19 13:10:39 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,14 +4,37 @@
 using Backend_SENAI.classes;
 
 PessoaFisica novaPf = new PessoaFisica();
+Endereco novoEndPf = new Endereco();
 
 novaPf.nome = "Giovana";
 novaPf.cpf = "12345678975";
 novaPf.rendimento = 7000.5f;
+novaPf.dataNasc = new DateTime(2000, 01, 01);
+
 Console.WriteLine($"Pessoa Fisica____________________________");
 float resultado = novaPf.CalcularImposto(novaPf.rendimento);
 
 Console.WriteLine(resultado);
+
+Console.WriteLine($"Validação das Datas ______________________");
+DateTime temp = new DateTime(2000, 01, 01);
+Console.WriteLine(novaPf.ValidarDataNasc("2004/12/08"));
+
+novoEndPf.logradouro = "Rua Niteroi";
+novoEndPf.numero = 180;
+novoEndPf.complemento = "Predio";
+novoEndPf.endComercial = true;
+
+novaPf.endereco = novoEndPf;
+
+Console.WriteLine(@$"
+Nome: {novaPf.nome}
+Endereço: {novaPf.endereco.logradouro}, Número: {novaPf.endereco.numero}
+Maior Idade: {novaPf.ValidarDataNasc(novaPf.dataNasc)}
+");
+
+
+
 
 // Console.WriteLine(novaPf.nome);
 // Console.WriteLine(novaPf.cpf);
@@ -30,4 +53,4 @@ PessoaJuridica novaPj = new PessoaJuridica();
 float impostoPagar = novaPj.CalcularImposto(10000.5f);
 Console.WriteLine($"{impostoPagar:0.0}");
 Console.WriteLine($"{impostoPagar.ToString("C")}");
-
+// C transforma para a moeda do país.
