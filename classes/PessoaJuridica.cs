6cd0f89731147f8ad93982c1b9d7163b2ed98085
Giovana-Manuquian <103048833+Giovana-Manuquian@users.noreply.github.com>
2022-12-26 23:42:59 -0300

    Update PessoaJuridica.cs

--- classes/PessoaJuridica.cs
+++ classes/PessoaJuridica.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Backend_SENAI.interfaces;
 
@@ -33,8 +34,33 @@ namespace Backend_SENAI.classes
         }
 
         public bool ValidarCnpj(string cnpj)
+        //xxxxxxxx0001xx  - 14
+        //xx.xxx.xxx/0001-xx  - 18
+
         {
-            throw new NotImplementedException();
+            bool retornoCnpjValido = Regex.IsMatch(cnpj, @"^(\d{14})|(\d{2}.\d{3}.\d{3}/d{4}-\{2})$");
+            // "D" = decimais (0 a 9)/ d14 = validar 14 valores de 0 a 9.
+            //@ indica que o comando usando "\" é uma string, pois normalmente o "\" é usado em comandos.
+
+            if (retornoCnpjValido)
+            {
+                string substringCnpj14 = cnpj.Substring(8, 4); //validar as infomrações do cnpj.
+
+                if (substringCnpj14 == "0001")
+                {
+                    return true;
+                }
+            }
+
+            string substringCnpj18 = cnpj.Substring(11, 4);
+
+            if (substringCnpj18 == "0001")
+            {
+                return true;
+            }
+
+            return false;
+
         }
     }
 }
