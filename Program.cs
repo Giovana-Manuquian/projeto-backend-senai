36a53e15fe6a47f6f4c764e4aba0b952acad9ca9
Giovana-Manuquian <103048833+Giovana-Manuquian@users.noreply.github.com>
2022-12-26 23:44:02 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,42 +3,115 @@
 
 using Backend_SENAI.classes;
 
-PessoaFisica novaPf = new PessoaFisica();
-Endereco novoEndPf = new Endereco();
+Console.Clear();
 
-novaPf.nome = "Giovana";
-novaPf.cpf = "12345678975";
-novaPf.rendimento = 7000.5f;
-novaPf.dataNasc = new DateTime(2000, 01, 01);
+Utils.BarraCarregamento("Iniciando", 10, ".");
 
-Console.WriteLine($"Pessoa Fisica____________________________");
-float resultado = novaPf.CalcularImposto(novaPf.rendimento);
+string? opcao;
+do
+{
+    Console.Clear();
+    Console.WriteLine(@$"
 
-Console.WriteLine(resultado);
-
-Console.WriteLine($"Validação das Datas ______________________");
-DateTime temp = new DateTime(2000, 01, 01);
-Console.WriteLine(novaPf.ValidarDataNasc("2004/12/08"));
-
-novoEndPf.logradouro = "Rua Niteroi";
-novoEndPf.numero = 180;
-novoEndPf.complemento = "Predio";
-novoEndPf.endComercial = true;
-
-novaPf.endereco = novoEndPf;
-
-Console.WriteLine(@$"
-Nome: {novaPf.nome}
-Endereço: {novaPf.endereco.logradouro}, Número: {novaPf.endereco.numero}
-Maior Idade: {novaPf.ValidarDataNasc(novaPf.dataNasc)}
+            ***********************************************
+            |                                             |
+            |      Bem vindo(a) ao Sistema de Cadastro    |
+            |        de Pessoas Físicas e Jurídicas.      |
+            |                                             |
+            ***********************************************
+            |        Digite o número da opção desejada:   |
+            ***********************************************
+            |                                             |
+            |             1 - Pessoa Física               |
+            |             2 - Pessoa Jurídica             |
+            |                                             |
+            |             0 - Sair                        |
+            ***********************************************
 ");
 
+    opcao = Console.ReadLine();
+    Thread.Sleep(1000);
 
+    switch (opcao)
+    {
+        case "1":
 
+            PessoaFisica novaPf = new PessoaFisica();
+            Endereco novoEndPf = new Endereco();
 
-// Console.WriteLine(novaPf.nome);
-// Console.WriteLine(novaPf.cpf);
-// Console.WriteLine($"------------------------");
+            novaPf.nome = "Giovana";
+            novaPf.cpf = "12345678975";
+            novaPf.rendimento = 7000.5f;
+            novaPf.dataNasc = new DateTime(2000, 01, 01);
+
+            float resultado = novaPf.CalcularImposto(novaPf.rendimento);
+            Console.WriteLine(resultado);
+            Console.WriteLine($"<----Validação das Datas---->");
+            DateTime temp = new DateTime(2000, 01, 01);
+            Console.WriteLine(novaPf.ValidarDataNasc("2004/12/08"));
+
+            novoEndPf.logradouro = "Rua Niteroi";
+            novoEndPf.numero = 180;
+            novoEndPf.complemento = "Predio";
+            novoEndPf.endComercial = true;
+            novaPf.endereco = novoEndPf;
+
+            Console.WriteLine(@$"
+            Nome: {novaPf.nome}
+            Endereço: {novaPf.endereco.logradouro}, Número: {novaPf.endereco.numero}
+            Maior Idade: {novaPf.ValidarDataNasc(novaPf.dataNasc)}
+            ");
+
+            Console.WriteLine($"Para continuar, tecle Enter");
+            Console.ReadLine();
+
+            break;
+
+        case "2":
+
+            PessoaJuridica novaPj = new PessoaJuridica();
+            Endereco novoEndPj = new Endereco();
+
+            novaPj.cnpj = "21.137.998./0001-40";
+            Console.WriteLine(novaPj.ValidarCnpj("21.137.998./0001-40"));
+
+            Console.WriteLine(@$"
+            CNPJ: {novaPj.cnpj}
+            Válido: {novaPj.ValidarCnpj(novaPj.cnpj)}
+            ");
+
+            novoEndPj.logradouro = "Rua Sergipe";
+            novoEndPj.numero = 200;
+            novoEndPj.complemento = "Predio";
+            novoEndPj.endComercial = true;
+            novaPj.endereco = novoEndPj;
+
+            float impostoPagar = novaPj.CalcularImposto(10000.5f);
+            Console.WriteLine($"{impostoPagar:0.0}");
+            Console.WriteLine($"{impostoPagar.ToString("C")}");
+
+            Console.WriteLine($"Para continuar, tecle Enter");
+            Console.ReadLine();
+
+            break;
+
+        case "0":
+
+            Console.WriteLine($"Obrigado(a) por utilizar nosso sistema.");
+
+            break;
+
+        default:
+
+            Console.WriteLine($"A opção selecionada está incorreta, digite um valor indicado");
+
+            break;
+    }
+
+} while (opcao != "0");
+
+Utils.BarraCarregamento("Encerrando", 10, ".");
+
 
 
 // Console.WriteLine($"Nome {novaPf.nome}, CPF: {novaPf.cpf}");
@@ -47,10 +120,4 @@ Maior Idade: {novaPf.ValidarDataNasc(novaPf.dataNasc)}
 // Console.WriteLine("Bem Vindo" + novaPf.nome + ", CPF" + novaPf.cpf + "");
 // concatenação
 
-Console.WriteLine($"Pessoa Juridica____________________________");
-PessoaJuridica novaPj = new PessoaJuridica();
-
-float impostoPagar = novaPj.CalcularImposto(10000.5f);
-Console.WriteLine($"{impostoPagar:0.0}");
-Console.WriteLine($"{impostoPagar.ToString("C")}");
 // C transforma para a moeda do país.
